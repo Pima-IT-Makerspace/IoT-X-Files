ee1d194aa45f14c67e9ba0a86e84eeac44a34b8f
Shred3r <jbejarano005@gmail.com>
2025-08-27 12:27:17 -0700

    The Birth of Speedy

---
+++ Speedy/README.md
@@ -0,0 +1,4 @@
+Wifi car controlled by a gamepad, supports most controllers, tested on xbox controller.
+
+IMPORTANT
+Wifi credentials are unset, make sure to change them according to your network
---
+++ Speedy/speedy.ino
@@ -0,0 +1,287 @@
+#include <WiFi.h>
+#include <WebServer.h>
+
+// Replace with your network credentials
+const char* ssid     = "testlab-2";
+const char* password = "welcomewelcome";
+
+// Create an instance of the WebServer on port 80
+WebServer server(80);
+
+// Motor 1
+int motor1Pin1 = 27;
+int motor1Pin2 = 26;
+int enable1Pin = 14;
+
+// Motor 2
+int motor2Pin1 = 25;
+int motor2Pin2 = 33;
+int enable2Pin = 32;
+
+// Setting PWM properties
+const int freq = 30000;
+const int resolution = 8;
+int dutyCycle = 0;
+
+String valueString = String(0);
+
+void handleHTML() {
+  const char html[] PROGMEM = R"rawliteral(
+  <!DOCTYPE HTML>
+  <html>
+    <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="icon" href="data:,">
+      <style>
+        html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center; }
+        .button { -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; background-color: #4CAF50; border: none; color: white; padding: 12px 28px; text-decoration: none; font-size: 26px; margin: 1px; cursor: pointer; }
+        .button2 {background-color: #555555;}
+      </style>
+    </head>
+    
+    <body>
+      <h1>Speedy v1</h1>
+      <p id="gamepad-info">Waiting for Gamepad.</p>
+      <p id="button-info">Button Pressed:.</p>
+      
+      <p><button class="button" onclick="moveForward()">FORWARD</button></p>
+      <div style="clear: both;">
+        <p>
+          <button class="button" onclick="moveLeft()">LEFT</button>
+          <button class="button button2" onclick="stopRobot()">STOP</button>
+          <button class="button" onclick="moveRight()">RIGHT</button>
+        </p>
+      </div>
+      <p><button class="button" onclick="moveReverse()">REVERSE</button></p>
+      <p>Motor Speed: <span id="motorSpeed">0</span></p>
+      <input type="range" min="0" max="100" step="25" id="motorSlider" oninput="updateMotorSpeed(this.value)" value="0"/>
+    </body>
+
+    <script>
+      var CurrentSpeed = 0;
+      // Allows our functions to be called through our buttons
+      function moveForward() { fetch('/forward'); }
+      function moveLeft() { fetch('/left'); }
+      function stopRobot() { fetch('/stop'); }
+      function moveRight() { fetch('/right'); }
+      function moveReverse() { fetch('/reverse'); }
+
+      buttonInfo = document.getElementById("button-info");
+
+      function updateMotorSpeed(pos) {
+        document.getElementById('motorSpeed').innerHTML = pos;
+        fetch(`/speed?value=${pos}`);
+      }
+
+      var gamepadInfo;
+
+      //Returns a true or false if a button is pressed on the gamepad
+      function buttonPressed(b) {
+        if (typeof(b) == "object") {
+          
+          return b.pressed;
+        }
+        return b == 1.0;
+      }
+      
+      // A function to handle button presses and send commands
+      function handleGamepadInput() {
+        
+        var gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads : []);
+        if (!gamepads) return;
+
+        var gp = gamepads[0];
+        if (!gp) return; // Make sure a gamepad is actually connected
+
+        // Check for button presses and send commands Need to make this either on hold, or reduce speed or increase on another button1
+        //Why isn't this a for loop?
+        // for (let button = 0; button < gp.buttons.length; button++) {
+        //   if (buttonPressed(gp.buttons[button])) {
+        //     buttonInfo.innerHTML = "button pressed " + button;
+        //     break;
+        //   }
+        // }
+        if (buttonPressed(gp.buttons[0])) { // A button
+          moveReverse();
+          buttonInfo.innerHTML = "Reverse ";
+        } else if (buttonPressed(gp.buttons[1])) { // B button
+          moveRight();
+          buttonInfo.innerHTML = "Right ";
+        } else if (buttonPressed(gp.buttons[2])) { // X button
+          moveForward();
+          buttonInfo.innerHTML = "Forward ";
+        } else if (buttonPressed(gp.buttons[3])) { // Y button
+          moveLeft();
+          buttonInfo.innerHTML = "Left";
+        } 
+        else if (buttonPressed(gp.buttons[5])) { //RT
+          if(CurrentSpeed <= 200)
+          {
+            CurrentSpeed += 25;
+            fetch(`/speed?value=${CurrentSpeed}`);
+            buttonInfo.innerHTML = "Speed at " + CurrentSpeed;
+          } else
+          {
+            buttonInfo.innerHTML = "Stop you are already at 200" + CurrentSpeed;
+          }
+        }
+        else if (buttonPressed(gp.buttons[4])){
+          CurrentSpeed -= 25;
+          fetch(`/speed?value=${CurrentSpeed}`);
+        }
+      }
+      
+      // Make sure the DOM is loaded before accessing elements
+      window.onload = function() {
+        gamepadInfo = document.getElementById("gamepad-info");
+
+        window.addEventListener("gamepadconnected", function(e) {
+          var gp = e.gamepad;
+          gamepadInfo.innerHTML = "Gamepad connected at index " + gp.index + ": " + gp.id + ". It has " + gp.buttons.length + " buttons and " + gp.axes.length + " axes.";
+          handleGamepadInput();
+        });
+        
+        window.addEventListener("gamepaddisconnected", function(e) {
+          gamepadInfo.innerHTML = "disconnected.";
+        });
+
+        if (!('GamepadEvent' in window)) {
+          var interval = setInterval(pollGamepads, 500);
+        }
+
+        function pollGamepads() {
+          var gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads : []);
+          for (var i = 0; i < gamepads.length; i++) {
+            
+            var gp = gamepads[i];
+            if (gp) {
+              gamepadInfo.innerHTML = "Gamepad connected at index " + gp.index + ": " + gp.id + ". It has " + gp.buttons.length + " buttons and " + gp.axes.length + " axes.";
+              handleGamepadInput();
+              clearInterval(interval);
+            }
+          }
+        }
+
+        //Modifying this will change the amount of get requests used for checking if the button is pressed or not. 
+        //If an on hold effect is wanted, we'd want this value to be very low and constantly sending requests, however this will fluff network traffic
+        //Maybe use an if statement to limit this function from sending multiple requests
+        var Controllerinterval = setInterval(handleGamepadInput, 100);
+      };
+    </script>
+  </html>
+)rawliteral";
+  server.send(200, "text/html", html);
+}
+
+void handleForward() {
+  Serial.println("Forward");
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, HIGH);
+  digitalWrite(motor2Pin1, LOW);
+  digitalWrite(motor2Pin2, HIGH);
+  server.send(200);
+}
+
+void handleLeft() {
+  Serial.println("Left");
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, LOW);
+  digitalWrite(motor2Pin1, LOW);
+  digitalWrite(motor2Pin2, HIGH);
+  server.send(200);
+}
+
+void handleStop() {
+  Serial.println("Stop");
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, LOW);
+  digitalWrite(motor2Pin1, LOW);
+  digitalWrite(motor2Pin2, LOW);
+  server.send(200);
+}
+
+void handleRight() {
+  Serial.println("Right");
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, HIGH);
+  digitalWrite(motor2Pin1, LOW);
+  digitalWrite(motor2Pin2, LOW);
+  server.send(200);
+}
+
+void handleReverse() {
+  Serial.println("Reverse");
+  digitalWrite(motor1Pin1, HIGH);
+  digitalWrite(motor1Pin2, LOW);
+  digitalWrite(motor2Pin1, HIGH);
+  digitalWrite(motor2Pin2, LOW);
+  server.send(200);
+}
+
+void handleSpeed() {
+  if (server.hasArg("value")) {
+    valueString = server.arg("value");
+    int value = valueString.toInt();
+    if (value == 0) {
+      ledcWrite(enable1Pin, 0);
+      ledcWrite(enable2Pin, 0);
+      digitalWrite(motor1Pin1, LOW);
+      digitalWrite(motor1Pin2, LOW);
+      digitalWrite(motor2Pin1, LOW);
+      digitalWrite(motor2Pin2, LOW);
+    } else {
+      dutyCycle = map(value, 25, 200, 200, 255);
+      ledcWrite(enable1Pin, dutyCycle);
+      ledcWrite(enable2Pin, dutyCycle);
+      Serial.println("Motor speed set to " + String(value));
+    }
+  }
+  server.send(200);
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  // Set the Motor pins as outputs
+  pinMode(motor1Pin1, OUTPUT);
+  pinMode(motor1Pin2, OUTPUT);
+  pinMode(motor2Pin1, OUTPUT);
+  pinMode(motor2Pin2, OUTPUT);
+
+  // Configure PWM Pins
+  ledcAttach(enable1Pin, freq, resolution);
+  ledcAttach(enable2Pin, freq, resolution);
+
+  // Initialize PWM with 0 duty cycle
+  ledcWrite(enable1Pin, 0);
+  ledcWrite(enable2Pin, 0);
+
+  // Connect to Wi-Fi
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Define routes
+  server.on("/", handleHTML);
+  server.on("/forward", handleForward);
+  server.on("/left", handleLeft);
+  server.on("/stop", handleStop);
+  server.on("/right", handleRight);
+  server.on("/reverse", handleReverse);
+  server.on("/speed", handleSpeed);
+
+  // Start the server
+  server.begin();
+}
+
+void loop() {
+  server.handleClient();
+}
